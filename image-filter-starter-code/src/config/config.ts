5dfc250646c076b879cc25ca26ea433204ad02c3
eedygreen <idris.isah2@st.futminna.edu.ng>
2021-04-15 16:31:06 +0100

    main: server.ts

--- image-filter-starter-code/src/config/config.ts
+++ image-filter-starter-code/src/config/config.ts
@@ -2,10 +2,10 @@ import { props } from "bluebird";
 
 export const config = {
     "dev": {
-        "username": process.env.POSTGRESS_USERNAME,
-        "password": process.env.POSTGRESS_PASSWORD,
-        "database": process.env.POSTGRESS_DATABASE,
-        "host": process.env.POSGTRESS_HOST,
+        // "username": process.env.POSTGRESS_USERNAME,
+        // "password": process.env.POSTGRESS_PASSWORD,
+        // "database": process.env.POSTGRESS_DATABASE,
+        // "host": process.env.POSGTRESS_HOST,
         //"dialect": "postgres",
         "aws_region": process.env.AWS_REGION,
         "aws_profile": process.env.AWS_PROFILE,
